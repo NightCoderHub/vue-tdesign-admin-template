79198784feffb9dd759c10793246b7cc610aa762
Li_Xun <321731029@qq.com>
2025-05-28 23:41:09 +0800

    fix: 修改main.js的bug

--- src/main.js
+++ src/main.js
@@ -7,13 +7,13 @@ import "tdesign-vue-next/es/style/index.css";
 import "@/style/index.scss";
 
 import "./permission";
+const app = createApp(App);
 
 import directives from "./directives";
 Object.keys(directives).forEach((key) => {
   app.directive(key, directives[key]);
 });
 
-const app = createApp(App);
 app.use(store);
 app.use(router);
 app.mount("#app");
